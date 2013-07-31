589cf56b0fd6644ad7c58af8a8cd666f2bc6f318
Windows 7 <Windows 7@Windows7-PC>
2013-07-31 17:20:43 +0530

    outer2

---
+++ outer/src/Oueter.java
@@ -0,0 +1,16 @@
+
+public class Oueter {int width; 
+int height;
+int depth;
+
+public int volume(int x,int y,int z)
+{
+	return width *height *depth;
+}
+
+public void test() {
+	// TODO Auto-generated method stub
+	
+}
+
+}
