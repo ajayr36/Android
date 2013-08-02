c967fd27f3617f1e7f8579cbcfa049968a1ee152
Windows 7 <Windows 7@Windows7-PC>
2013-08-02 22:21:29 +0530

    Sree Ganesh

---
+++ outer/src/outer.java
@@ -0,0 +1,27 @@
+
+ class outer {
+	
+int outer_x = 100;
+void test() {
+for(int i=0; i<10; i++) {
+class Inner {
+void display() {
+System.out.println("display: outer_x = " + outer_x);
+}
+}
+Inner inner = new Inner();
+inner.display();
+}
+}
+}
+class InnerClassDemo{
+	public static void main (String args[]){
+		Oueter outer = new Oueter();
+		outer.test();}
+	}
+
+
+
+	
+
+
